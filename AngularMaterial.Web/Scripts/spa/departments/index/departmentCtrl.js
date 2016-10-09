0fa35128204e4c4a305d6b9383f896098a07abc0
Wanasak <u510610433@gmail.com>
2016-10-09 16:36:09 +0700

    commit
    Add option order department

--- AngularMaterial.Web/Scripts/spa/departments/index/departmentCtrl.js
+++ AngularMaterial.Web/Scripts/spa/departments/index/departmentCtrl.js
@@ -8,12 +8,17 @@
     function departmentCtrl($scope, $http) {
 
         $scope.departments = [];
+        $scope.orderDepartment = 'Name';
+        $scope.onClickOrderDepartment = onClickOrderDepartment;
 
         function loadDepartments() {
             $http.get("api/departments", null)
                 .then(function (result) {
                     $scope.departments = result.data;
                 }, function (response) {});
+        }
+        function onClickOrderDepartment(order) {
+            $scope.orderDepartment = (order === 'aesc') ? 'Name' : '-Name';
         }
 
         loadDepartments();
